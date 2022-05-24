183b5b33fe5f35eabfecb3dbfaca7fd82933071d
singcl <iambabyer@gamil.com>
2022-05-24 23:53:33 +0800

    chore: 项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <Counter />
+  </view>
+</template>
+
+<script>
+import './index.styl'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Counter
+  }
+}
+</script>
