9b0995f8a217e7cff1d0c4e7936ff86bd7fe5cb2
singcl <iambabyer@gamil.com>
2022-05-27 23:51:40 +0800

    feat: format prettier

--- global.d.ts
+++ global.d.ts
@@ -12,7 +12,15 @@ declare module '*.styl';
 // @ts-ignore
 declare const process: {
   env: {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
+    TARO_ENV:
+      | 'weapp'
+      | 'swan'
+      | 'alipay'
+      | 'h5'
+      | 'rn'
+      | 'tt'
+      | 'quickapp'
+      | 'qq';
     [key: string]: any;
-  }
-}
+  };
+};
