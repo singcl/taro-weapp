8f2d7723d71d02803980cb16cdc1c751f6c5b06e
singcl <iambabyer@gamil.com>
2022-05-27 22:26:11 +0800

    feat: eslint 集成prettier 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,23 @@
 // https://eslint.vuejs.org/user-guide/#editor-integrations
 module.exports = {
   root: true,
-  // lint vue 文件
-  // eslint-config-taro配置包中没有引入依赖eslint-plugin-vue
-  // 所以需要项目安装eslint-plugin-vue
-  extends: ["taro/vue3"],
-  rules: { semi: ["error", "never"] },
+
+  extends: [
+    // lint vue 文件
+    // eslint-config-taro配置包中没有引入依赖eslint-plugin-vue
+    // 所以需要项目安装eslint-plugin-vue
+    "taro/vue3",
+    // eslint 集成prettier 规则，
+    // 需要先安装
+    // eslint-config-prettier
+    // eslint-plugin-prettier
+    // 1. disabled掉eslint中和prettier想冲突的规则
+    // 2. 实时以eslint 提示的方式显示prettier规则
+    // @see https://github.com/prettier/eslint-plugin-prettier
+    "plugin:prettier/recommended",
+  ],
+  rules: {},
+  // 方式一：
   // lint js?(x) ts?(x)文件
   // eslint-config-taro 没有引入相关依赖
   // 需要项目单独安装：
@@ -14,6 +26,7 @@ module.exports = {
   // "eslint-plugin-react": "^7.30.0",
   // "eslint-plugin-react-hooks": "^4.5.0",
   overrides: [{ files: ["**/*.js?(x)", "**/*.ts?(x)"], extends: ["taro"] }],
+  // 方式二：
   // 不使用taro配置包，自定义lint ts文件
   // overrides: [
   //   {
@@ -33,4 +46,4 @@ module.exports = {
   //     excludedFiles: "*.test.ts",
   //   },
   // ],
-};
+}
