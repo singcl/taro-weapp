317e3d235ad54e2c302b9f4d777111093c6172e9
singcl <1276837689@qq.com>
2022-06-20 22:15:24 +0800

    feat(api): 登录接口调试

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,14 @@
 <template>
   <view class="index">
     <XmCounter />
-    <button type="primary" :class="styles['button-sp-area']">主要按钮</button>
+    <button type="primary" :class="styles['button-sp-area']" @tap="handleLogin">
+      登录
+    </button>
   </view>
 </template>
 
 <script>
+import Taro from '@tarojs/taro';
 import styles from './index.module.styl';
 import XmCounter from '@/components/XmCounter';
 
@@ -16,6 +19,24 @@ export default {
   },
   computed: {
     styles: () => styles,
+  },
+  methods: {
+    async handleLogin() {
+      const { code } = await Taro.login();
+      try {
+        Taro.showLoading();
+        await Taro.request({
+          url: 'http://127.0.0.1:9000/weixin/login',
+          method: 'GET',
+          data: { code },
+          header: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+          },
+        });
+      } finally {
+        Taro.hideLoading();
+      }
+    },
   },
 };
 </script>
