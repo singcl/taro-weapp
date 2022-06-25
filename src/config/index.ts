bf391cbdfe258f9e6074c263c6e75d8ded58901a
singcl <1276837689@qq.com>
2022-06-25 17:06:31 +0800

    feat(http): axios http service client

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ export default {
     dev: 'http://localhost:3000',
     pro: 'xxx',
   },
+  // 请求时不需要拦截的路径
   publicPath: [/^\/public/, /^\/login/],
 };
--- src/services/http.ts
+++ src/services/http.ts
@@ -1,9 +1,11 @@
-// import Taro from '@tarojs/taro';
+import Taro from '@tarojs/taro';
 // import { HTTP_STATUS } from '../constants/status';
-// import publicConfig from '@/config/index';
-// import { errorHandler } from '@/common';
+import publicConfig from '@/config/index';
+import { errorHandler } from '@/common';
 // import axios from 'axios-miniprogram';
-// const CancelToken = axios.CancelToken;
+import axios, { AxiosInstance } from 'axios';
+
+const CancelToken = axios.CancelToken;
 
 //
 class HttpRequest {
@@ -25,10 +27,38 @@ class HttpRequest {
 
   // 移除请求状态
   removePending(key: string, isRequest = false) {
-    if (this.pending[key] && isRequest) {
+    if (isRequest && this.pending[key]) {
       this.pending[key]('取消重复请求');
     }
     delete this.pending[key];
+  }
+
+  // 设定拦截器
+  interceptors(instance: AxiosInstance) {
+    instance.interceptors.request.use(
+      (config) => {
+        console.log('kiko :>> ', config);
+        let isPublic = false;
+        publicConfig.publicPath.forEach((path) => {
+          const reqUrl = config.url || '';
+          isPublic = isPublic || path.test(reqUrl);
+        });
+        const token = Taro.getStorageSync('token');
+        if (token && isPublic) {
+          config.headers && (config.headers.Token = token);
+        }
+        const key = `${config.url}&${config.method}`;
+        this.removePending(key, true);
+        config.cancelToken = new CancelToken((cancel) => {
+          this.pending[key] = cancel;
+        });
+        return config;
+      },
+      (err) => {
+        errorHandler(err);
+        return Promise.reject(err);
+      }
+    );
   }
 }
 
