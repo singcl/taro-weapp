8cec2b1156d9e09fb3219f224e84a970d4b6801a
singcl <1276837689@qq.com>
2022-06-27 01:41:58 +0800

    feat(http): header token

--- src/services/interceptors/headers.ts
+++ src/services/interceptors/headers.ts
@@ -1,9 +1,12 @@
-import { Chain } from '@tarojs/taro';
+import Taro, { Chain } from '@tarojs/taro';
 
 export default function headersInterceptor(chain: Chain) {
   const requestParams = chain.requestParams;
   const { header = {} } = requestParams;
   header['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+
+  const token = Taro.getStorageSync('token');
+  if (token) header['Token'] = token;
 
   return chain.proceed(Object.assign(requestParams, { header }));
   //   .then((res) => {
