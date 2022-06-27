3708166ffe1cea2e8b1600e52fdfd051e7e6f478
singcl <1276837689@qq.com>
2022-06-28 00:23:40 +0800

    feat(page): about user

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="index">
     <XmCounter />
     <button type="primary" :class="styles['button-sp-area']" @tap="handleLogin">
-      登录
+      获取用户信息
     </button>
     <text v-if="loginData">{{ loginData.code }}</text>
   </view>
@@ -10,12 +10,9 @@
 
 <script lang="ts">
 import Taro from '@tarojs/taro';
+// import API from '@/api';
 import styles from './index.module.styl';
 import XmCounter from '@/components/XmCounter/index.vue';
-
-interface LoginResponse {
-  code: string;
-}
 
 export default {
   name: 'XmIndex',
@@ -32,28 +29,8 @@ export default {
   },
   methods: {
     async handleLogin() {
-      const { code } = await Taro.login();
-      try {
-        Taro.showLoading();
-        const { data: loginData } = await Taro.request<LoginResponse>({
-          // 使用局域网IP地址，方便在手机上也可以访问网络
-          // 手机上无法访问时，请检查防火墙是否关闭，或者是否配置正确
-          url: 'http://192.168.2.101:9000/weixin/login',
-          method: 'GET',
-          data: { code },
-          header: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-          },
-        });
-        this.loginData = loginData;
-        Taro.showToast({
-          title: `登录成功`,
-          icon: 'success',
-          duration: 2000,
-        });
-      } finally {
-        Taro.hideLoading();
-      }
+      const token = Taro.getStorageSync('token');
+      console.log('token', token);
     },
   },
 };
