2a43026f19f5039799cb5529841b7106216ca8f0
singcl <1276837689@qq.com>
2022-06-28 23:03:14 +0800

    feat(pages): mine center user info

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <view>个人中心</view>
+  <view>
+    <!-- https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/userProfile.html -->
+    <!-- https://developers.weixin.qq.com/miniprogram/dev/api/open-api/user-info/wx.getUserProfile.html -->
+    <button
+      class="avatar-wrapper"
+      open-type="chooseAvatar"
+      v-on:chooseavatar="onChooseAvatar"
+    >
+      <image class="avatar" :src="wxAvatarUrl"></image>
+    </button>
+    <input type="nickname" class="weui-input" placeholder="请输入昵称" />
+    <button open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">
+      获取手机号
+    </button>
+    <!-- https://developers.weixin.qq.com/community/develop/doc/000e881c7046a8fa1f4d464105b001 -->
+    <open-data type="userAvatarUrl"></open-data>
+    <open-data type="userNickName"></open-data>
+  </view>
 </template>
 
 <script lang="ts">
@@ -9,5 +26,18 @@ export default {
 </script>
 
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+const defaultAvatarUrl =
+  'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0';
+// 获取微信头像
+const wxAvatarUrl = ref(defaultAvatarUrl);
+function onChooseAvatar(e: any) {
+  const { avatarUrl } = e.detail;
+  wxAvatarUrl.value = avatarUrl;
+}
+
+// 获取手机号
+function getPhoneNumber(e: any) {
+  console.log(e.detail.code);
+}
 </script>
