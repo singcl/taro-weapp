a5e54581775c436b2c2ab1d5bcb4c36e25364525
singcl <1276837689@qq.com>
2022-06-29 17:09:25 +0800

    feat(app): adjust the app constructor

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import API from '@/api';
+
+export default defineComponent({
+  onShow(options) {
+    //
+    console.log('----', options);
+  },
+  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+  //
+  onLaunch() {
+    // 登录
+    API.auth.TaroLogin();
+  },
+});
+</script>
--- src/app.ts
+++ src/app.ts
@@ -4,28 +4,18 @@ import { Button, Cell, Icon } from '@nutui/nutui-taro';
 // 定制化主题必须使用 scss
 import '@nutui/nutui-taro/dist/styles/themes/default.scss';
 // import Taro from '@tarojs/taro';
+
 import '@/services/http';
 import { /* useApp, useAuth, */ store } from '@/stores';
-import API from '@/api';
+import App from './App';
 
 import './app.styl';
 
-const App = createApp({
-  onShow(options) {
-    //
-    console.log('----', options);
-  },
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-  //
-  onLaunch() {
-    // 登录
-    API.auth.TaroLogin();
-  },
-});
+const app = createApp(App);
 
-App.use(store).use(Button).use(Cell).use(Icon);
+app.use(store).use(Button).use(Cell).use(Icon);
 
 // const auth = useAuth();
 // const app = useApp();
 
-export default App;
+export default app;
