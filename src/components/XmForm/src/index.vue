f1d87f3eeba87498e15665b97e0b4fea9cb5a775
singcl <1276837689@qq.com>
2022-06-29 22:56:25 +0800

    feat(components): components

---
+++ src/components/XmForm/src/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <nut-form>
+    <nut-form-item label="姓名">
+      <input class="nut-input-text" placeholder="请输入姓名" type="text" />
+    </nut-form-item>
+    <nut-form-item label="年龄">
+      <input class="nut-input-text" placeholder="请输入年龄" type="text" />
+    </nut-form-item>
+    <nut-form-item label="联系电话">
+      <input class="nut-input-text" placeholder="请输入联系电话" type="text" />
+    </nut-form-item>
+    <nut-form-item label="地址">
+      <input class="nut-input-text" placeholder="请输入地址" type="text" />
+    </nut-form-item>
+    <nut-form-item label="备注">
+      <nut-textarea placeholder="请输入备注" type="text" />
+    </nut-form-item>
+  </nut-form>
+</template>
+<script lang="ts">
+export default {
+  name: 'XmForm',
+};
+</script>
+<script setup lang="ts"></script>
