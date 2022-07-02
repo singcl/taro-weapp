9b20c8959b122e28f2cf563e152efcbe2f83c21b
singcl <1276837689@qq.com>
2022-07-02 14:17:08 +0800

    chore(vitest): vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [vue()],
+  test: {
+    // 启用类似 jest 的全局测试 API
+    globals: true,
+    // 使用 happy-dom 模拟 DOM
+    // 这需要你安装 happy-dom 作为对等依赖（peer dependency）
+    environment: 'happy-dom',
+  },
+});
