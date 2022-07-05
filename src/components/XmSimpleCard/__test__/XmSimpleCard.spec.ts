40896d8b3d9873629a7db0441a53d60be901ef0b
singcl <1276837689@qq.com>
2022-07-05 11:01:13 +0800

    feat(componenst): simple card component

---
+++ src/components/XmSimpleCard/__test__/XmSimpleCard.spec.ts
@@ -0,0 +1,15 @@
+// XmSimpleCard.test.js
+import { render } from '@testing-library/vue';
+
+import XmSimpleCard from '@/components/XmSimpleCard';
+
+//
+test('it should work', async () => {
+  render(XmSimpleCard, {
+    props: {
+      /* ... */
+    },
+  });
+
+  // TODO 插槽 Test
+});
---
+++ src/components/XmSimpleCard/src/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view :class="styles['wrapper']">
+    <slot></slot>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'XmSimpleCard',
+});
+</script>
+
+<script lang="ts" setup>
+import styles from './index.module.styl';
+// TODO its value not never used
+</script>
