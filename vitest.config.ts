2fd1263a2f940e1c8ad53313c17d78d70401c9ee
singcl <1276837689@qq.com>
2022-07-06 01:00:25 +0800

    fix(vitest):🐞@import "~@/styles/theme/vars"
    TEST FAILED OUTPUT:
    src/components/XmWallet/__test__/XmSimpleCard.spec.ts [ src/components/XmWallet/__test__/XmSimpleCard.spec.ts ]
    ReferenceError: ~/xmiles/taro-weapp/src/components/XmWallet/src/xmWallet.module.styl:1:9
       1| @import "~@/styles/theme/vars"
    --------------^
       2|
       3| .wrapper
       4|   position relative
    
    aliases is not defined
    
    //  RESOLUTION:
    //  @ see https://github.com/vuejs/component-compiler-utils/issues/49

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,56 @@
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
+import { Evaluator } from 'stylus';
+
+// aliases
+const aliases = {
+  '@/': `${path.resolve(__dirname, 'src')}/`,
+  '@/styles': `${path.resolve(__dirname, 'src')}/styles`,
+};
+
+// ===================================================================
+/* TEST FAILED OUTPUT:
+src/components/XmWallet/__test__/XmSimpleCard.spec.ts [ src/components/XmWallet/__test__/XmSimpleCard.spec.ts ]
+ReferenceError: ~/xmiles/taro-weapp/src/components/XmWallet/src/xmWallet.module.styl:1:9
+   1| @import "~@/styles/theme/vars"
+--------------^
+   2|
+   3| .wrapper
+   4|   position relative
+
+aliases is not defined */
+
+//  RESOLUTION:
+//  @ see https://github.com/vuejs/component-compiler-utils/issues/49
+const visitImport = Evaluator.prototype.visitImport;
+Evaluator.prototype.visitImport = function (imported) {
+  const path2 = imported.path.first;
+
+  if (path2.string.startsWith('~')) {
+    const alias = Object.keys(aliases).find((entry) => path2.string.startsWith(`~${entry}`));
+
+    if (alias) path2.string = path2.string.substr(1).replace(alias, aliases[alias]);
+  }
+
+  return visitImport.call(this, imported);
+};
+
+// ===================================================================
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
+    alias: aliases,
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      style: {
+        preprocessOptions: {
+          stylus: { Evaluator },
+        },
+      },
+    }),
+  ],
   test: {
     // 启用类似 jest 的全局测试 API
     globals: true,
