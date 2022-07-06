33b5d831cb648b47c5fa8b64a7c414250f86b134
singcl <1276837689@qq.com>
2022-07-07 01:06:29 +0800

    feat(nutui): nutui components

--- src/components/nutui/index.ts
+++ src/components/nutui/index.ts
@@ -1,9 +1,21 @@
 import type { App } from 'vue';
 // 定制化主题必须使用 scss
 import '@nutui/nutui-taro/dist/styles/themes/default.scss';
-import { Button, Cell, Icon, Form, FormItem, CellGroup, TextArea, Avatar, Badge } from '@nutui/nutui-taro';
+import {
+  Button,
+  Cell,
+  Icon,
+  Form,
+  FormItem,
+  CellGroup,
+  TextArea,
+  Avatar,
+  Badge,
+  Swiper,
+  SwiperItem,
+} from '@nutui/nutui-taro';
 
-const components = [Button, Cell, Icon, Form, FormItem, CellGroup, TextArea, Avatar, Badge];
+const components = [Button, Cell, Icon, Form, FormItem, CellGroup, TextArea, Avatar, Badge, Swiper, SwiperItem];
 
 export default class NutUI {
   static install(Vue: App<Element> /* options */) {
