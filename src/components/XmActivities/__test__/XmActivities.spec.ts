1325a78a347f840e9dd126c44abbfbfdc6ac3e53
singcl <1276837689@qq.com>
2022-07-08 01:02:25 +0800

    test(components): ✅ XmActivities component unit t

--- src/components/XmActivities/__test__/XmActivities.spec.ts
+++ src/components/XmActivities/__test__/XmActivities.spec.ts
@@ -3,11 +3,23 @@ import { render } from '@testing-library/vue';
 
 import XmActivities from '@/components/XmActivities';
 
+const activities = [
+  { icon: 'dingdan', value: '红包', title: '我的订单', color: '#ff8201', size: '28' },
+  { icon: 'daifukuan', value: '现金', title: '待付款', color: '#ff8201', size: '28' },
+];
+
 describe('XmActivities Props', () => {
   test('Default Props', async () => {
-    const { getByText } = render(XmActivities);
-    getByText('元');
-    getByText(0.0);
-    getByText(0);
+    render(XmActivities, { props: { activities: [] } });
+  });
+});
+
+describe('XmActivities activities Props', () => {
+  test('Default Props', async () => {
+    const { getByText } = render(XmActivities, { props: { activities: activities } });
+    getByText('我的订单');
+    getByText('待付款');
+    // getByText('红包'); // TODO
+    // getByText('现金'); // TODO
   });
 });
