8b0c5ed1b40593f5af2d370eb9f637940a272093
singcl <1276837689@qq.com>
2022-07-10 16:07:30 +0800

    feat(store): declaring this in function

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia';
+import { defineStore, Store } from 'pinia';
 import Taro from '@tarojs/taro';
 import { LOGIN_STATUS, LOGIN_TOKEN } from '@/constants';
 import API from '@/api';
@@ -30,6 +30,8 @@ interface AuthGetters<S = AuthState> extends Record<string, ((s: S) => any) | ((
   token: () => string;
 }
 
+type StoreInstance = Store<'authInfo', AuthState, AuthGetters, AuthActions>;
+
 export const useAuthStore = defineStore<string, AuthState, AuthGetters, AuthActions>({
   id: 'authInfo',
   state: () => ({
@@ -52,19 +54,16 @@ export const useAuthStore = defineStore<string, AuthState, AuthGetters, AuthActi
     async login() {
       return await API.auth.TaroLogin();
     },
-    // 操作可以通过 this 访问 whole store instance 并提供完整类型（和自动完成✨）支持。
-    // @see https://pinia.web3doc.top/core-concepts/actions.html
-    // 但是并没有类型提示 TODO:
-    // async loginExpired() {
-    //   await this.login();
-    // },
     // 重新登录获取token
     async refreshTokenFunc() {
       Taro.removeStorageSync(LOGIN_TOKEN);
       return await API.auth.TaroLogin();
     },
-    //
-    async reConnect(params: any) {
+    // 操作可以通过 this 访问 whole store instance 并提供完整类型（和自动完成✨）支持。
+    // @see https://pinia.web3doc.top/core-concepts/actions.html
+    // @see https://www.tslang.cn/docs/handbook/functions.html `this参数`
+    // 重连
+    async reConnect(this: StoreInstance, params: any) {
       // 如果页面同时多个接口返回登录过期，确保重新登录接口只调用一次
       if (this.loginStatus !== LOGIN_STATUS.LOGIN_PENDING) {
         this.loginStatus = LOGIN_STATUS.LOGIN_PENDING;
